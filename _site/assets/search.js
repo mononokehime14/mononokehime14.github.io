e3c3c6a21b5858dbe5675c53cdce0771e5bbd32c
mononokehime14 <952284692@qq.com>
2023-03-08 10:33:32 -0500

    intern memo

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"写在最初，03/07/2023",'url':"/2023/03/07/first-post.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"写在最初，03/07/2023",'url':"/2023/03/07/first-post.html"},{'title':"实习找工回忆录",'url':"/2023/03/07/intern-memo.html"}]};
